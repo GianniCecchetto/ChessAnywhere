f73c567d1d110e91e90e5e8455aa91030c700579
Gianni <giannicecchetto59@gmail.com>
2025-08-20 15:21:25 +0200

    Added base function to try ci

---
+++ python-app/tests/test_chess_anywhere_api.py
@@ -0,0 +1,21 @@
+import pytest
+import requests
+from unittest.mock import patch
+from app.api import fetch_data
+
+def test_fetch_data_success():
+    mock_response = {"message": "Hello Client"}
+
+    with patch("app.api.requests.get") as mock_get:
+        mock_get.return_value.json.return_value = mock_response
+        mock_get.return_value.status_code = 200
+        result = fetch_data("http://fake-server.com", "status")
+
+    assert result == {"message": "Hello Client"}
+
+def test_fetch_data_error():
+    with patch("app.api.requests.get") as mock_get:
+        mock_get.side_effect = requests.exceptions.ConnectionError
+
+        with pytest.raises(requests.exceptions.ConnectionError):
+            fetch_data("http://fake-server.com", "status")
