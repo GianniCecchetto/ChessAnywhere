5032a53503d0eae43cb22b33b60604de6c6dbf36
Gianni <giannicecchetto59@gmail.com>
2025-08-20 16:04:10 +0200

    Adjusted path in tests

--- python-app/test/test_chess_anywhere_api.py
+++ python-app/test/test_chess_anywhere_api.py
@@ -6,7 +6,7 @@ from app.chess_anywhere_api import fetch_data
 def test_fetch_data_success():
     mock_response = {"message": "Hello Client"}
 
-    with patch("app.api.requests.get") as mock_get:
+    with patch("app.chess_anywhere_api.requests.get") as mock_get:
         mock_get.return_value.json.return_value = mock_response
         mock_get.return_value.status_code = 200
         result = fetch_data("http://fake-server.com", "status")
@@ -14,7 +14,7 @@ def test_fetch_data_success():
     assert result == {"message": "Hello Client"}
 
 def test_fetch_data_error():
-    with patch("app.api.requests.get") as mock_get:
+    with patch("app.chess_anywhere_api.requests.get") as mock_get:
         mock_get.side_effect = requests.exceptions.ConnectionError
 
         with pytest.raises(requests.exceptions.ConnectionError):
