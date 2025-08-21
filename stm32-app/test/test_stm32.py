355b018b2d2bae088f1f630d84a66a38e747c8bb
TschantzN <145683961+TschantzN@users.noreply.github.com>
2025-08-21 18:48:10 +0200

    Update test_stm32.py

--- stm32-app/test/test_stm32.py
+++ stm32-app/test/test_stm32.py
@@ -1,10 +1,5 @@
 import pytest
-import requests
-from unittest.mock import patch
-from app.chess_anywhere_api import fetch_data
-from app.chess_anywhere_api import send_data
 
-def test_fetch_data_success():
+def Test_uart():
     result = True
-
-assert result == True
+    assert result == True
