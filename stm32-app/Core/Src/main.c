8e14a6518d5b6d7ca566c338571fa12bd265e4c5
thomasstaheli <thomas_s@bluewin.ch>
2025-08-25 11:55:42 +0200

    Test expect false

--- stm32-app/Core/Src/main.c
+++ stm32-app/Core/Src/main.c
@@ -1,5 +1,5 @@
 /* USER CODE BEGIN Header */
-/**
+/** 
   ******************************************************************************
   * @file           : main.c
   * @brief          : Main program body
@@ -92,8 +92,8 @@ int main(void)
   MX_USART2_UART_Init();
   UART_Flush(&huart2);
   /* USER CODE BEGIN 2 */
-  char msg[] = "Hello World !\r\n";
-  // char msg[] = "Z\r\n";
+  // char msg[] = "Hello World !\r\n";
+  char msg[] = "Z\r\n";
   HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
   /* USER CODE END 2 */
 
