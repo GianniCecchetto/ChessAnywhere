d6de225d77f950b96945b95668d9d3b3287fec90
nathan.tschantz <nathan.tschantz@hes-so.ch>
2025-08-26 11:28:51 +0200

    postion des coup possibles et des piece mirroir selon couleur, debut logique partie

--- app/app/gui/draw_board.py
+++ app/app/gui/draw_board.py
@@ -13,7 +13,7 @@ PIECES_MAP = {
     'P': 'wP', 'N': 'wN', 'B': 'wB', 'R': 'wR', 'Q': 'wQ', 'K': 'wK'
 }
 
-def draw_chessboard(parent, size=8, square_size=70, board=None, playable_square=None):
+def draw_chessboard(parent, size=8, square_size=70, board=None, playable_square=None,player_color = True):
     """
     Dessine un échiquier avec les pièces.
     """
@@ -28,28 +28,37 @@ def draw_chessboard(parent, size=8, square_size=70, board=None, playable_square=
     
     # Dictionnaire pour stocker les références aux images
     piece_images = {}
-
+    print("========\n", player_color)
     for row in range(size):
         for col in range(size):
+            if player_color:
+                row_index = row
+                col_index = col
+            else:
+                row_index = 7 - row
+                col_index = 7 - col
+
+            square_color = colors[(row + col) % 2]
+            fill_color = square_color
+
+            if playable_square:
+                if playable_square[row_index][col_index] == "X":
+                    fill_color = "#FF0000"
+                elif playable_square[row_index][col_index] == "O":
+                    fill_color = "#0026FF"
+                elif playable_square[row_index][col_index] == "P":
+                    fill_color = "#00FBFF"
+
             x1 = col * square_size
             y1 = row * square_size
             x2 = x1 + square_size
             y2 = y1 + square_size
-            
-            square_color = colors[(row + col) % 2]
-            fill_color = square_color
-            
-            if playable_square:
-                if playable_square[row][col] == "X":
-                    fill_color = "#FF0000"
-                elif playable_square[row][col] == "O":
-                    fill_color = "#0026FF"
-                elif playable_square[row][col] == "P":
-                    fill_color = "#00FBFF"
 
             canvas.create_rectangle(x1, y1, x2, y2, fill=square_color, outline="", width=OUTLINE_WIDTH)
-            if playable_square and playable_square[row][col] != ".":
-                canvas.create_rectangle(x1 + 6, y1 + 6, x2 - 6, y2 - 6, fill=fill_color, width=OUTLINE_WIDTH, outline="gray")
+
+            if playable_square and playable_square[row_index][col_index] != ".":
+                canvas.create_rectangle(x1 + 6, y1 + 6, x2 - 6, y2 - 6, 
+                                        fill=fill_color, width=OUTLINE_WIDTH, outline="gray")
 
     # ----- dessiner les pièces -----
     if board:
@@ -58,8 +67,12 @@ def draw_chessboard(parent, size=8, square_size=70, board=None, playable_square=
             if piece:
                 row, col = divmod(square, 8)
                 
-                y_coord = (7 - row) * square_size
-                x_coord = col * square_size
+                if player_color :
+                    y_coord = (7 - row) * square_size
+                    x_coord = col * square_size
+                else :
+                    y_coord = (row) * square_size
+                    x_coord = (7-col) * square_size
 
                 piece_name = PIECES_MAP[piece.symbol()]
                 image_path = f"{PIECES_PATH}{piece_name}.png"
--- app/app/gui/join_game.py
+++ app/app/gui/join_game.py
@@ -15,13 +15,26 @@ def join_online_game(board_container,game_name):
         f"msg: {test}"
     )
     board = chess.Board()
-    online_game_loop(board_container,board)
+    online_game_loop(board_container,board,player_color)
+
+def create_online_game(board_container,game_name):
+    test = mylib.get_msg(game_name.encode("utf-8")).decode("utf-8")
+    messagebox.showinfo(
+        "Rejoindre une partie en ligne",
+        f"Vous avez rejoint la partie : {game_name}\n"
+        f"msg: {test}"
+    )
+    # recuperer la couleur, par défaut BLACK
+    player_color = chess.BLACK
+    board = chess.Board()
+    online_game_loop(board_container,board,player_color)
 
 def create_local_game(board_container):
+    player_color = chess.WHITE
     messagebox.showinfo(
         "Création de la partie local"
     )
     board = chess.Board()
-    local_game_loop(board_container,board)
+    local_game_loop(board_container,board,player_color)
 
 
