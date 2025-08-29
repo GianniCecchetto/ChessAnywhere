bf41282c1d5c9372d4ef20aca63ac6be27398edf
thomasstaheli <thomas_s@bluewin.ch>
2025-08-29 21:15:20 +0200

    Working UART

--- firmware/Core/Inc/rgb_led.h
+++ firmware/Core/Inc/rgb_led.h
@@ -12,44 +12,16 @@
 #define HIGH_DUTY  26   // ≈ 0.8 µs (logique "1")
 #define LOW_DUTY   13   // ≈ 0.4 µs (logique "0")
 
-typedef enum {
-    RED,
-    GREEN,
-    BLUE,
-    YELLOW,
-    CYAN,
-    MAGENTA,
-    WHITE
-} ColorName;
-
-typedef struct {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-} Color;
-
-static const Color colors_values[] = {
-		{255, 0,   0},
-		{0,   255, 0},
-		{0,   0,   255},
-		{255, 255, 0},
-		{0,   255, 255},
-		{255, 0,   255},
-		{255, 255, 255},
-};
-
 static volatile uint8_t ws2812_transfer_complete = 0;
 
 // -------------------------------------------------------------------
 // Fill the buffer PWM depending on the table "colors"
 // -------------------------------------------------------------------
-void rgb_update_buffer(uint16_t *pwm_data, ColorName *colors);
+// void rgb_update_buffer(uint16_t *pwm_data, ColorName *colors);
 
-/* OLD
 // -------------------------------------------------------------------
 // Fill the buffer PWM depending on the table "colors"
 // -------------------------------------------------------------------
 void rgb_update_buffer(uint16_t *pwm_data, uint8_t colors[][3]);
-*/
 
 #endif
--- firmware/Core/Src/rgb_led.c
+++ firmware/Core/Src/rgb_led.c
@@ -1,13 +1,14 @@
 #include "rgb_led.h"
 
+/*
 // -------------------------------------------------------------------
 // Fill the buffer PWM depending on the table "colors"
 // -------------------------------------------------------------------
 void rgb_update_buffer(uint16_t *pwm_data, ColorName *colors) {
     for(int led = 0; led < LED_NUMBER; led++)
     {
-        uint32_t color = ((uint32_t)colors_values[colors[led]].r << 16) |  // R
-        				 ((uint32_t)colors_values[colors[led]].g << 8) |  // G
+        uint32_t color = ((uint32_t)colors_values[colors[led]].g << 16) |  // R
+        				 ((uint32_t)colors_values[colors[led]].r << 8) |  // G
                          ((uint32_t)colors_values[colors[led]].b << 0);  // R
         for(int i = 0; i < 24; i++)
         {
@@ -24,8 +25,8 @@ void rgb_update_buffer(uint16_t *pwm_data, ColorName *colors) {
     	pwm_data[i] = 0;
     }
 }
+*/
 
-/* OLD
 // -------------------------------------------------------------------
 // Fill the buffer PWM depending on the table "colors"
 // -------------------------------------------------------------------
@@ -51,4 +52,3 @@ void rgb_update_buffer(uint16_t *pwm_data, uint8_t colors[][3])
     	pwm_data[i] = 0;
     }
 }
-*/
